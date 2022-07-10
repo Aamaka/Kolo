11eabc124094bbe1bc1b049d0b3cc11184fc0e82
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-11 00:11:36 +0100

    My Money

--- src/main/java/africa/semicolon/dto/requests/WithdrawRequest.java
+++ src/main/java/africa/semicolon/dto/requests/WithdrawRequest.java
@@ -1,4 +1,14 @@
 package africa.semicolon.dto.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WithdrawRequest {
+    private String accountNumber;
+    private int amount;
+    private String password;
 }
--- src/main/java/africa/semicolon/dto/responses/WithdrawResponse.java
+++ src/main/java/africa/semicolon/dto/responses/WithdrawResponse.java
@@ -1,4 +1,13 @@
 package africa.semicolon.dto.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WithdrawResponse {
+    private String message;
+    private int accountBalance;
 }
