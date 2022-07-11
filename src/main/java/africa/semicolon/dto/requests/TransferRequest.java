71c72580a07d3025185c02dd72f6e59d879f7c3a
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-07-11 18:42:17 +0100

    Ego

--- src/main/java/africa/semicolon/dto/requests/TransferRequest.java
+++ src/main/java/africa/semicolon/dto/requests/TransferRequest.java
@@ -1,4 +1,16 @@
 package africa.semicolon.dto.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransferRequest {
+    private String senderAccount;
+    private String receiverAccount;
+    private int amount;
+    private String password;
+
 }
